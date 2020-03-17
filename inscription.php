8de1153b56cf1155d1d198fb4c15539f0c450470
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-03-17 12:47:38 +0100

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+</head>
+<body>
+
+	<h1>Inscription :</h1>
+
+	<label for="pseudo">Pseudo :</label>
+	<input type="text" name="pseudo" id="pseudo" placeholder="Choisir un pseudo" required autofocus=""> <br>
+
+	<label for="mdp">Mot de passe :</label>
+	<input type="text" name="mdp" id="mdp" placeholder="Choisir un mot de passe" required> <br>
+
+	<button>Valider</button>
+
+</body>
+</html>
